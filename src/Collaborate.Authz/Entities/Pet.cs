e79fb6c53c277b32628f84b6a014e6658e7b90ec
Marco Hernandez <marcohern@gmail.com>
2026-08-25 11:04:21 -0500

    Added Pet entity to be used as an example for a storage model

---
+++ src/Collaborate.Authz/Entities/Pet.cs
@@ -0,0 +1,10 @@
+﻿namespace Collaborate.Authz.Entities
+{
+    public class Pet
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Species { get; set; } = string.Empty;
+        public DateTime? BirthDate { get; set; }
+    }
+}
